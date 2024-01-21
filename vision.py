7b38b8110da7afea7dbb8b9ebe39590ced63b6d3
Sam <sam20060223@gmail.com>
2024-01-21 21:24:58 +0800

    add test and remove comment

--- vision.py
+++ vision.py
@@ -1,14 +1,11 @@
-import time
-
 import cv2
 import numpy as np
-import os
 import tensorflow as tf
 from PIL import Image
 
 import colorsys
 import copy
-import time, sys
+import time, sys, json
 import threading
 
 from PyQt5 import QtWidgets, QtCore
@@ -16,8 +13,6 @@ from PyQt5.QtGui import *
 
 from nets.deeplab import Deeplabv3
 from utils.utils import cvtColor, preprocess_input, resize_image
-
-import json
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
@@ -141,72 +136,36 @@ def putInformation(frame):
   2, (255, 255, 255), 8, cv2.LINE_AA)
 
     return frame
+
+colors = [ (0, 0, 0), (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128), (0, 128, 128), 
+                            (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0), (192, 128, 0), (64, 0, 128), (192, 0, 128), 
+                            (64, 128, 128), (192, 128, 128), (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128), 
+                            (128, 64, 12)]
 class DeeplabV3(object):
     _defaults = {
-        #-------------------------------------------------------------------#
-        #   model_path指向logs文件夹下的权值文件
-        #   训练好后logs文件夹下存在多个权值文件，选择验证集损失较低的即可。
-        #   验证集损失较低不代表miou较高，仅代表该权值在验证集上泛化性能较好。
-        #-------------------------------------------------------------------#
         "model_path"        : 'model/ep084-loss0.216-val_loss0.116.h5',
-        #----------------------------------------#
-        #   所需要区分的类的个数+1
-        #----------------------------------------#
         "num_classes"       : 7,
-        #----------------------------------------#
-        #   所使用的的主干网络：mobilenet、xception    
-        #----------------------------------------#
         "backbone"          : "mobilenet",
-        #----------------------------------------#
-        #   输入图片的大小
-        #----------------------------------------#
         "input_shape"       : [512, 512],
-        #----------------------------------------#
-        #   下采样的倍数，一般可选的为8和16
-        #   与训练时设置的一样即可
-        #----------------------------------------#
         "downsample_factor" : 16,
-        #-------------------------------------------------#
-        #   mix_type参数用于控制检测结果的可视化方式
-        #
-        #   mix_type = 0的时候代表原图与生成的图进行混合
-        #   mix_type = 1的时候代表仅保留生成的图
-        #   mix_type = 2的时候代表仅扣去背景，仅保留原图中的目标
-        #-------------------------------------------------#
         "mix_type"          : 0,
     }
-
-    #---------------------------------------------------#
-    #   初始化Deeplab
-    #---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
         for name, value in kwargs.items():
             setattr(self, name, value)
-        #---------------------------------------------------#
-        #   画框设置不同的颜色
-        #---------------------------------------------------#
+
         if self.num_classes <= 21:
-            self.colors = [ (0, 0, 0), (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128), (0, 128, 128), 
-                            (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0), (192, 128, 0), (64, 0, 128), (192, 0, 128), 
-                            (64, 128, 128), (192, 128, 128), (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128), 
-                            (128, 64, 12)]
+            self.colors = colors
         else:
             hsv_tuples = [(x / self.num_classes, 1., 1.) for x in range(self.num_classes)]
             self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
             self.colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), self.colors))
-        #---------------------------------------------------#
-        #   获得模型
-        #---------------------------------------------------#
+
         self.generate()
 
-    #---------------------------------------------------#
-    #   获得所有的分类
-    #---------------------------------------------------#
     def generate(self):
-        #-------------------------------#
-        #   载入模型与权值
-        #-------------------------------#
+
         self.model = Deeplabv3([self.input_shape[0], self.input_shape[1], 3], self.num_classes,
                                 backbone = self.backbone, downsample_factor = self.downsample_factor)
 
@@ -218,48 +177,25 @@ class DeeplabV3(object):
         pr = self.model(image_data, training=False)
         # print(type(pr))
         return pr
-    #---------------------------------------------------#
-    #   检测图片
-    #---------------------------------------------------#
     def detect_image(self, image):
-        #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-        #---------------------------------------------------------#
         image       = cvtColor(image)
-        #---------------------------------------------------#
-        #   对输入图像进行一个备份，后面用于绘图
-        #---------------------------------------------------#
+
         old_img     = copy.deepcopy(image)
         orininal_h  = np.array(image).shape[0]
         orininal_w  = np.array(image).shape[1]
-        #---------------------------------------------------------#
-        #   给图像增加灰条，实现不失真的resize
-        #   也可以直接resize进行识别 
-        #---------------------------------------------------------#
+
         image_data, nw, nh  = resize_image(image, (self.input_shape[1], self.input_shape[0]))
-        #---------------------------------------------------------#
-        #   归一化+通道数调整到第一维度+添加上batch_size维度
-        #---------------------------------------------------------#
+
         image_data  = np.expand_dims(preprocess_input(np.array(image_data, np.float32)), 0)
 
-        #---------------------------------------------------#
-        #   图片传入网络进行预测
-        #---------------------------------------------------#
         pr = self.get_pred(image_data)[0].numpy()
         # print(type(pr))
-        #---------------------------------------------------#
-        #   将灰条部分截取掉
-        #---------------------------------------------------#
+
         pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
                 int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
-        #---------------------------------------------------#
-        #   进行图片的resize
-        #---------------------------------------------------#
+
         pr = cv2.resize(pr, (orininal_w, orininal_h), interpolation = cv2.INTER_LINEAR)
-        #---------------------------------------------------#
-        #   取出每一个像素点的种类
-        #---------------------------------------------------#
+
         pr = pr.argmax(axis=-1)
         # thread = threading.Thread(processPredict(pr, orininal_w, orininal_h))
         # thread.start()
@@ -274,14 +210,13 @@ class DeeplabV3(object):
             #     seg_img[:, :, 1] += ((pr[:, :] == c ) * self.colors[c][1]).astype('uint8')
             #     seg_img[:, :, 2] += ((pr[:, :] == c ) * self.colors[c][2]).astype('uint8')
             seg_img = np.reshape(np.array(self.colors, np.uint8)[np.reshape(pr, [-1])], [orininal_h, orininal_w, -1])
-            #------------------------------------------------#
-            #   将新图片转换成Image的形式
-            #------------------------------------------------#
-            image   = Image.fromarray(np.uint8(seg_img))
+
+            # kernel = np.ones((7,7),np.uint8)
+            # seg_img = cv2.dilate(seg_img,kernel,iterations = 5)
+            # seg_img = cv2.erode(seg_img,kernel,iterations = 5)
             getEdge(seg_img[data["middlePointY"]])
-            #------------------------------------------------#
-            #   将新图与原图及进行混合
-            #------------------------------------------------#
+            image   = Image.fromarray(np.uint8(seg_img))
+
             image   = Image.blend(old_img, image, 0.7)
 
         elif self.mix_type == 1:
@@ -291,17 +226,15 @@ class DeeplabV3(object):
             #     seg_img[:, :, 1] += ((pr[:, :] == c ) * self.colors[c][1]).astype('uint8')
             #     seg_img[:, :, 2] += ((pr[:, :] == c ) * self.colors[c][2]).astype('uint8')
             seg_img = np.reshape(np.array(self.colors, np.uint8)[np.reshape(pr, [-1])], [orininal_h, orininal_w, -1])
-            #------------------------------------------------#
-            #   将新图片转换成Image的形式
-            #------------------------------------------------#
+
             image   = Image.fromarray(np.uint8(seg_img))
+            getEdge(seg_img[data["middlePointY"]])
 
         elif self.mix_type == 2:
             seg_img = (np.expand_dims(pr != 0, -1) * np.array(old_img, np.float32)).astype('uint8')
-            #------------------------------------------------#
-            #   将新图片转换成Image的形式
-            #------------------------------------------------#
+
             image = Image.fromarray(np.uint8(seg_img))
+            getEdge(seg_img[data["middlePointY"]])
 
         return image
 
@@ -311,12 +244,12 @@ for gpu in gpus:
     
 deeplab = DeeplabV3()
 
-video_path      = "/Users/sam/Documents/MyProject/mixProject/TYAIcar/MLtraning/visualIdentityVideo/IMG_9522.MOV"
+video_path      = "/Users/sam/Documents/MyProject/mixProject/TYAIcar/MLtraning/visualIdentityVideo/IMG_9590.MOV"
 video_save_path = ""
 video_fps       = 60.0
 
 def opencv():
-    global ocv,video_path
+    global ocv,video_path,video_save_path,video_fps
     
     capture=cv2.VideoCapture(video_path)
     if video_save_path!="":
@@ -326,7 +259,7 @@ def opencv():
 
     ref, frame = capture.read()
     if not ref:
-        raise ValueError("未能正确读取摄像头（视频），请注意是否正确安装摄像头（是否正确填写视频路径）。")
+        raise ValueError("Video source Error")
 
     fps = 0.0
     while(ocv):
@@ -338,8 +271,8 @@ def opencv():
         frame = Image.fromarray(np.uint8(frame))
         frame = np.array(deeplab.detect_image(frame))
         frame = putInformation(frame)
-        frame = cv2.resize(frame, (720, 405))
-        height, width, channel = frame.shape
+        height, width, channel = 405, 720, 3
+        frame = cv2.resize(frame, (width, height))
         bytesPerline = channel * width
 
         img = QImage(frame, width, height, bytesPerline, QImage.Format_RGB888)
