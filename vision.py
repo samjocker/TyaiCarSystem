3d361861b4d6d92f9359bf9ad587979d43c1fff6
Sam <sam20060223@gmail.com>
2024-01-25 15:13:04 +0800

    add side slider

--- vision.py
+++ vision.py
@@ -18,7 +18,7 @@ app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 MainWindow.setObjectName("MainWindow")
 MainWindow.setWindowTitle("TYAI car")
-MainWindow.resize(720, 565)
+MainWindow.resize(720, 595)
 
 TrapezoidWindow = QtWidgets.QMainWindow()
 TrapezoidWindow.setObjectName("TrapezoidWindow")
@@ -65,61 +65,91 @@ font.setPointSize(18)
 datumYvalue.setText("555")
 datumYvalue.setFont(font)
 
+sideDistSlider = QtWidgets.QSlider(MainWindow)
+sideDistSlider.setGeometry(110, 490, 380, 30)
+sideDistSlider.setOrientation(QtCore.Qt.Horizontal)
+sideDistSlider.setMaximum(960)
+sideDistTitle = QtWidgets.QLabel(MainWindow)
+sideDistTitle.setGeometry(70, 487, 80, 30)
+font = QFont() 
+font.setPointSize(18)
+sideDistTitle.setText("邊距")
+sideDistTitle.setFont(font)
+sideDistValue = QtWidgets.QLabel(MainWindow)
+sideDistValue.setGeometry(500, 487, 80, 30)
+font = QFont() 
+font.setPointSize(18)
+sideDistValue.setText("555")
+sideDistValue.setFont(font)
+
+sideButton = QtWidgets.QPushButton(MainWindow)
+sideButton.setGeometry(535, 487, 80, 30)
+sideButton.setText("Right")
+sideButtonState = True
+
 trapezoidXvalue = QtWidgets.QSlider(MainWindow)
-trapezoidXvalue.setGeometry(110, 490, 500, 30)
+trapezoidXvalue.setGeometry(110, 520, 500, 30)
 trapezoidXvalue.setOrientation(QtCore.Qt.Horizontal)
 trapezoidXvalue.setMaximum(960)
 trapezoidXtitle = QtWidgets.QLabel(MainWindow)
-trapezoidXtitle.setGeometry(70, 487, 80, 30)
+trapezoidXtitle.setGeometry(70, 517, 80, 30)
 font = QFont() 
 font.setPointSize(18)
 trapezoidXtitle.setText("梯X")
 trapezoidXtitle.setFont(font)
 trapezoidXnum = QtWidgets.QLabel(MainWindow)
-trapezoidXnum.setGeometry(620, 487, 80, 30)
+trapezoidXnum.setGeometry(620, 517, 80, 30)
 font = QFont() 
 font.setPointSize(18)
 trapezoidXnum.setText("555")
 trapezoidXnum.setFont(font)
 
 trapezoidYvalue = QtWidgets.QSlider(MainWindow)
-trapezoidYvalue.setGeometry(110, 520, 500, 30)
+trapezoidYvalue.setGeometry(110, 550, 500, 30)
 trapezoidYvalue.setOrientation(QtCore.Qt.Horizontal)
 trapezoidYvalue.setMaximum(540)
 trapezoidYtitle = QtWidgets.QLabel(MainWindow)
-trapezoidYtitle.setGeometry(70, 517, 80, 30)
+trapezoidYtitle.setGeometry(70, 547, 80, 30)
 font = QFont() 
 font.setPointSize(18)
 trapezoidYtitle.setText("梯Y")
 trapezoidYtitle.setFont(font)
 trapezoidYnum = QtWidgets.QLabel(MainWindow)
-trapezoidYnum.setGeometry(620, 517, 80, 30)
+trapezoidYnum.setGeometry(620, 547, 80, 30)
 font = QFont() 
 font.setPointSize(18)
 trapezoidYnum.setText("555")
 trapezoidYnum.setFont(font)
+
+colors = [ (255, 255, 255), (150, 150, 150), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128), (0, 128, 128), 
+                            (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0), (192, 128, 0), (64, 0, 128), (192, 0, 128), 
+                            (64, 128, 128), (192, 128, 128), (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128), 
+                            (128, 64, 12)]
 
 try:
     with open("setting.json", 'r') as file:
         data = json.load(file)
         datumXslider.setValue(data["middlePointX"])
         datumYslider.setValue(data["middlePointY"])
+        sideDistSlider.setValue(data["sideDistValue"])
         trapezoidXvalue.setValue(data["trapezoidXvalue"])
         trapezoidYvalue.setValue(data["trapezoidYvalue"])
         datumXvalue.setText(str(data["middlePointX"]))
         datumYvalue.setText(str(data["middlePointY"]))
+        sideDistValue.setText(str(data["sideDistValue"]))
         trapezoidXnum.setText(str(data["trapezoidXvalue"]))
         trapezoidYnum.setText(str(data["trapezoidYvalue"]))
 except FileNotFoundError:
-    data = {"middlePointX": 0, "middlePointY": 0, "trapezoidXvalue": 0, "trapezoidYvalue": 0}
+    data = {"middlePointX": 0, "middlePointY": 0, "sideDistValue":0, "trapezoidXvalue": 0, "trapezoidYvalue": 0}
     datumXvalue.setText(str(0))
     datumYvalue.setText(str(0))
+    sideDistValue.setText(str(0))
     trapezoidXnum.setText(str(0))
     trapezoidYnum.setText(str(0))
     with open("setting.json", 'w') as file:
         json.dump(data, file)
 except KeyError:
-    data = {"middlePointX": 0, "middlePointY": 0, "trapezoidXvalue": 0, "trapezoidYvalue": 0}
+    data = {"middlePointX": 0, "middlePointY": 0, "sideDistValue":0, "trapezoidXvalue": 0, "trapezoidYvalue": 0}
     datumXvalue.setText(str(0))
     datumYvalue.setText(str(0))
     trapezoidXnum.setText(str(0))
@@ -144,6 +174,21 @@ def datumYsliderValue(value):
     datumYvalue.setText(str(data["middlePointY"]))
 datumYslider.valueChanged.connect(datumYsliderValue)
 
+def sideDistSliderValue(value):
+    global data
+    data["sideDistValue"] = value
+    sideDistValue.setText(str(data["sideDistValue"]))
+sideDistSlider.valueChanged.connect(sideDistSliderValue)
+
+def sideButtonClicked():
+    global sideButtonState
+    if sideButtonState:
+        sideButton.setText("Left")
+    else:
+        sideButton.setText("Right")
+    sideButtonState = not sideButtonState
+sideButton.clicked.connect(sideButtonClicked)
+
 def trapezoidXvalueChange(value):
     data["trapezoidXvalue"] = value
     trapezoidXnum.setText(str(value))
@@ -162,17 +207,17 @@ def closeOpenCV():
 MainWindow.closeEvent = closeOpenCV  
 
 def getEdge(pr):
-    global data
+    global data, colors
     leftOffset = 0
     rightOffset = 0
     for x in range(data["middlePointX"]):
-        if np.all(pr[data["middlePointX"]-x] == [128, 0, 0]):
+        if np.all(pr[data["middlePointX"]-x] == list(colors[1])):
             leftOffset += 1
         else:
             break
     prLong = len(pr)
     for x in range(prLong-data["middlePointX"]):
-        if np.all(pr[x+data["middlePointX"]-1] == [128, 0, 0]):
+        if np.all(pr[x+data["middlePointX"]-1] == list(colors[1])):
             rightOffset += 1
         else:
             break
@@ -189,7 +234,7 @@ shortcut1 = QtWidgets.QShortcut(QKeySequence("Ctrl+S"), MainWindow)
 shortcut1.activated.connect(save)
 
 def putInformation(frame):
-    global data, edge
+    global data, edge, sideButtonState
     height, width, channel = frame.shape
 
     srcPts = np.float32([[0,1079], [1919,1079], [data["trapezoidXvalue"], data["trapezoidYvalue"]], [1919-data["trapezoidXvalue"], data["trapezoidYvalue"]]])
@@ -201,6 +246,12 @@ def putInformation(frame):
     TestLabel.setPixmap(QPixmap.fromImage(img))
 
     frame = cv2.circle(frame, (data["middlePointX"], data["middlePointY"]), radius=5, color=(255,255,255), thickness=10)
+    sideCdn = ()
+    if sideButtonState:
+        sideCdn = (data["sideDistValue"]+data["middlePointX"], data["middlePointY"])
+    else:
+        sideCdn = (data["middlePointX"]-data["sideDistValue"], data["middlePointY"])
+    frame = cv2.circle(frame, sideCdn, radius=5, color=(250,149,55), thickness=20)
     frame = cv2.line(frame, (0,1079), (data["trapezoidXvalue"], data["trapezoidYvalue"]), (255,255,255), 2)
     frame = cv2.line(frame, (1919,1079), (1919-data["trapezoidXvalue"], data["trapezoidYvalue"]), (255,255,255), 2)
     frame = cv2.line(frame, (data["trapezoidXvalue"], data["trapezoidYvalue"]), (1919-data["trapezoidXvalue"], data["trapezoidYvalue"]), (255,255,255), 2)
@@ -217,18 +268,14 @@ def putInformation(frame):
 
     return frame
 
-colors = [ (0, 0, 0), (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128), (0, 128, 128), 
-                            (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0), (192, 128, 0), (64, 0, 128), (192, 0, 128), 
-                            (64, 128, 128), (192, 128, 128), (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128), 
-                            (128, 64, 12)]
 class DeeplabV3(object):
     _defaults = {
-        "model_path"        : 'model/ep098-loss0.220-val_loss0.099.h5',
+        "model_path"        : 'model/ep100-loss0.153-val_loss0.047.h5',
         "num_classes"       : 7,
         "backbone"          : "mobilenet",
         "input_shape"       : [512, 512],
         "downsample_factor" : 16,
-        "mix_type"          : 0,
+        "mix_type"          : 1,
     }
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
@@ -374,5 +421,5 @@ video = threading.Thread(target=opencv)
 video.start()
 
 MainWindow.show()
-TrapezoidWindow.show()
+# TrapezoidWindow.show()
 sys.exit(app.exec_())
